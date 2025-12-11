5dc36b4fbc7461acaf045345a76eb17c49a08707
slyeet03 <shobhit7419@gmail.com>
2025-12-11 18:38:51 +0530

    Update selection.rs

---
+++ src/editor/selection.rs
@@ -0,0 +1,60 @@
+use super::cursor::Cursor;
+
+pub struct Selection {
+    anchor: Anchor,
+    cursor: Cursor,
+}
+
+pub struct Anchor {
+    row: usize,
+    col: usize,
+}
+
+impl Selection {
+    pub fn clear(&mut self) {
+        self.anchor.row = self.cursor.row;
+        self.anchor.col = self.cursor.col;
+    }
+
+    pub fn expand_to(&mut self, row: usize, col: usize) {
+        self.cursor.row = row;
+        self.cursor.col = col;
+    }
+
+    pub fn collapse_to(&mut self, row: usize, col: usize) {
+        self.expand_to(row, col);
+        self.clear();
+    }
+
+    pub fn range(&self) -> (usize, usize, usize, usize) {
+        let mut start_row: usize;
+        let mut start_col: usize;
+        let mut end_row: usize;
+        let mut end_col: usize;
+
+        if self.anchor.row < self.cursor.row {
+            start_row = self.anchor.row;
+            start_col = self.anchor.col;
+            end_row = self.cursor.row;
+            end_col = self.cursor.col;
+        } else if self.anchor.row > self.cursor.row {
+            end_row = self.anchor.row;
+            end_col = self.anchor.col;
+            start_row = self.cursor.row;
+            start_col = self.cursor.col;
+        } else {
+            if self.anchor.col <= self.cursor.col {
+                start_row = self.anchor.row;
+                start_col = self.anchor.col;
+                end_row = self.cursor.row;
+                end_col = self.cursor.col;
+            } else {
+                end_row = self.anchor.row;
+                end_col = self.anchor.col;
+                start_row = self.cursor.row;
+                start_col = self.cursor.col;
+            }
+        }
+        (start_row, start_col, end_row, end_col)
+    }
+}
